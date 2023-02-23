4d8c757baae6951a6033a17766fa512890a99123
Suyash <mail.suyashgarg@gmail.com>
2023-02-23 20:12:01 +0530

    update

--- tests/test_tidypyspark.py
+++ tests/test_tidypyspark.py
@@ -25,7 +25,57 @@ def test_mutate(penguins_data):
   assert set(['bl_+_1', 'bl_+_1_by_2']).issubset(cns)
   
   spark.stop()
-  
+
+
+def test_to_pandas(penguins_data):
+    from pyspark.sql import SparkSession
+    import pyspark.sql.functions as F
+    spark = SparkSession.builder.getOrCreate()
+    import pyspark
+    # import pandas
+    pen = spark.read.csv(penguins_data, header=True).drop("_c0")
+    res = pen.ts.to_pandas()
+    assert isinstance(res, pandas.core.frame.DataFrame)
+
+    spark.stop()
+
+
+def test_rename(penguins_data):
+    from pyspark.sql import SparkSession
+    import pyspark.sql.functions as F
+    spark = SparkSession.builder.getOrCreate()
+    import pyspark
+    import pandas as pd
+    pen = spark.read.csv(penguins_data, header=True).drop("_c0")
+    res = pen.ts.rename({'species': 'species_2'})
+    cns = res.ts.colnames
+    assert isinstance(res, pyspark.sql.dataframe.DataFrame)
+    assert set(['species_2']).issubset(cns)
+    assert ['species'] not in cns
+    spark.stop()
+
+def is_sublist(subset_list, full_list):
+    if subset_list[0] in full_list:
+        start_index = full_list.index(subset_list[0])
+        for i in range(len(subset_list)):
+            if full_list[start_index+i] != subset_list[i]:
+                return False
+        return True
+    return False
+def test_relocate(penguins_data):
+    from pyspark.sql import SparkSession
+    import pyspark.sql.functions as F
+    spark = SparkSession.builder.getOrCreate()
+    import pyspark
+    import pandas as pd
+    pen = spark.read.csv(penguins_data, header=True).drop("_c0")
+    res = pen.ts.relocate(["island", "species"], after = "year")
+    cns = res.ts.colnames
+    print(cns)
+    assert isinstance(res, pyspark.sql.dataframe.DataFrame)
+    assert is_sublist(["year", "island", "species"],  cns)
+    spark.stop()
+
 
 def test_summarise(penguins_data):
   
