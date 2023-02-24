c6acc0692c798475ac04e97176829c5c55704a55
Suyash <mail.suyashgarg@gmail.com>
2023-02-24 12:24:46 +0530

    implemented: rename, relocate

--- src/tidypyspark/tidypyspark_class.py
+++ src/tidypyspark/tidypyspark_class.py
@@ -1,13 +1,13 @@
 # ----------------------------------------------------------------------------
 # This file is a part of tidypyspark python package
 # Find the dev version here: https://github.com/talegari/tidypyspark
-# ------------------------------------------------------------------------------
-
+# ---------------------------------------------------------------------
 import warnings
 import pyspark
 import pyspark.sql.functions as F
 from pyspark.sql.window import Window
 from tidypyspark.accessor_class import register_dataframe_accessor
+import numpy as np
 from tidypyspark._unexported_utils import (
                               _is_kwargable,
                               _is_valid_colname,
@@ -59,8 +59,8 @@ class acc_on_pyspark():
   @property
   def dim(self):
     return (self.nrow, self.ncol)
-      
-  # cleaners --------------------------------------------------------------
+
+# cleaners --------------------------------------------------------------
   
   def _clean_by(self, by):
     '''
@@ -486,7 +486,132 @@ class acc_on_pyspark():
       res = res.select(*cn)
     
     return res
-  
+
+  def rename(self, old_new_dict):
+    '''
+    rename
+    Rename columns of the pyspark dataframe
+
+    Parameters
+    ----------
+    old_new_dict: dict
+      A dict with old names as keys and new names as values
+
+    Returns
+    -------
+    pyspark dataframe
+
+    Examples
+    --------
+    pen.ts.rename({'species': 'species_2', "year":"year_2})
+
+    Invalid Example
+    pen.ts.rename({'species': 'species_2', "species_2":"species_3})
+    '''
+    cn = self.colnames
+
+    if (old_new_dict is None):
+      raise Exception("provide  atleast one column to change")
+
+    assert isinstance(old_new_dict, dict), \
+      "arg 'old_new_dict' should be a dict"
+    assert set(cn).issuperset(old_new_dict.keys()), \
+      "keys of the input dict should be existing column names"
+    assert _is_string_or_string_list(list(old_new_dict.values())), \
+      "values of the dict should be strings"
+    assert _is_unique_list(list(old_new_dict.values())), \
+      "values of the dict should be unique"
+    assert len(old_new_dict)>0, \
+        "there should alteast one element in old_new_dict"
+
+    # new names should not intersect with 'remaining' names
+    remaining = set(cn).difference(old_new_dict.keys())
+    assert len(remaining.intersection(old_new_dict.values())) == 0, \
+      ("New intended column names (values of the dict) lead to duplicate "
+       "column names"
+       )
+    select_col_with_alias = [F.col(c).alias(old_new_dict.get(c, c)) for c in self.colnames]
+    res = self.__data.select(select_col_with_alias)
+    return res
+
+  def relocate(self, column_names, before = None, after = None):
+    '''
+    relocate
+    Relocate the columns
+
+    Parameters
+  ----------
+    column_names : string or a list of strings
+    column names to be moved
+    before : string, optional
+    column before which the column are to be moved. The default is None.
+    after : string, optional
+    column after which the column are to be moved. The default is None.
+
+    Returns
+    -------
+    pyspark dataframe
+
+    Notes
+    -----
+    Only one among 'before' and 'after' can be not None. When both are None,
+    the columns are added to the beginning of the dataframe (leftmost)
+
+    Examples
+    --------
+    # move "island" and "species" columns to the left of the dataframe
+    pen.ts.relocate(["island", "species"])
+
+    # move "sex" and "year" columns to the left of "island" column
+    pen.ts.relocate(["sex", "year"], before = "island")
+
+    # move "island" and "species" columns to the right of "year" column
+    pen.ts.relocate(["island", "species"], after = "year")
+    '''
+
+    column_names = self._clean_column_names(column_names)
+    cn = self.colnames
+
+    cn = self.colnames
+    col_not_relocate = [i for i in cn if i not in column_names]
+
+    assert not ((before is not None) and (after is not None)), \
+    "Atleast one arg among 'before' and 'after' should be None"
+
+    if after is not None:
+      assert isinstance(after, str), \
+          "arg 'after' should be a string"
+      assert after in cn, \
+          "arg 'after' should be a exisiting column name"
+      assert not (after in column_names), \
+          "arg 'after' should be an element of 'column_names'"
+
+    if before is not None:
+      assert isinstance(before, str), \
+          "arg 'before' should be a string"
+      assert before in cn, \
+          "arg 'before' should be a exisiting column name"
+      assert not (before in column_names), \
+          "arg 'before' should be an element of 'column_names'"
+
+    # case 1: relocate to start when both before and after are None
+    if (before is None) and (after is None):
+      col_sequence = column_names + list(col_not_relocate)
+    elif (before is not None):
+      # case 2: before is not None
+      col_sequence = col_not_relocate[0:col_not_relocate.index(before)] + \
+                      column_names + \
+                      col_not_relocate[col_not_relocate.index(before):]
+    else:
+      # case 3: after is not None
+      after_index = col_not_relocate.index(after)
+      col_sequence = col_not_relocate[0:after_index+1] + \
+                     column_names + \
+                     col_not_relocate[after_index+1:]
+
+    res = self.__data.select(col_sequence)
+    return res
+
   def mutate(self, dictionary, window_spec = None, **kwargs):
     '''
     mutate
--- tests/test_tidypyspark.py
+++ tests/test_tidypyspark.py
@@ -1,7 +1,7 @@
 import pytest
 from tidypyspark.datasets import get_penguins_path
 import tidypyspark.tidypyspark_class as ts
-
+from tidypyspark._unexported_utils import _is_perfect_sublist
 
 @pytest.fixture
 def penguins_data():
@@ -19,13 +19,50 @@ def test_mutate(penguins_data):
                         'bl_+_1_by_2': F.col('bl_+_1') / 2}
                        )
          )
-         
   assert isinstance(res, pyspark.sql.dataframe.DataFrame)
   cns = res.ts.colnames
   assert set(['bl_+_1', 'bl_+_1_by_2']).issubset(cns)
   
   spark.stop()
-  
+
+def test_rename(penguins_data):
+    from pyspark.sql import SparkSession
+    import pyspark.sql.functions as F
+    spark = SparkSession.builder.getOrCreate()
+    import pyspark
+    pen = spark.read.csv(penguins_data, header=True).drop("_c0")
+    res = pen.ts.rename({'species': 'species_2'})
+    cns = res.ts.colnames
+    assert isinstance(res, pyspark.sql.dataframe.DataFrame)
+    assert set(['species_2']).issubset(cns)
+    assert ['species'] not in cns
+    spark.stop()
+
+def test_relocate(penguins_data):
+    from pyspark.sql import SparkSession
+    import pyspark.sql.functions as F
+    spark = SparkSession.builder.getOrCreate()
+    import pyspark
+    pen = spark.read.csv(penguins_data, header=True).drop("_c0")
+
+    #testing after clause
+    res = pen.ts.relocate(["island", "species"], after = "year")
+    cns = res.ts.colnames
+    assert isinstance(res, pyspark.sql.dataframe.DataFrame), "before clause failed 1"
+    assert _is_perfect_sublist(["year", "island", "species"],  cns), "after clause failed 2"
+
+    #testing before clause
+    res = pen.ts.relocate(["island", "species"], before = "year")
+    cns = res.ts.colnames
+    assert isinstance(res, pyspark.sql.dataframe.DataFrame), "before clause failed 1"
+    assert _is_perfect_sublist(["island", "species","year"],  cns), "before clause failed 2"
+
+    #testing without before-after clause
+    res = pen.ts.relocate(["bill_length_mm","bill_depth_mm"])
+    cns = res.ts.colnames
+    assert isinstance(res, pyspark.sql.dataframe.DataFrame), "without before-after clause failed 1"
+    assert _is_perfect_sublist(["bill_length_mm","bill_depth_mm", "species"],  cns), "without before-after clause failed 2"
+    spark.stop()
 
 def test_summarise(penguins_data):
   
